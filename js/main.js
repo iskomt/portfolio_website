858e4f0e7e6d8893d6fd4f0cc6634a06596c08a4
iskomt <iskomt@yahoo.com>
2019-05-30 22:52:44 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
+const btnLine = document.querySelector('.btn-line');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
@@ -13,6 +14,7 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
     if (!showMenu) {
         menuBtn.classList.add('close');
+        btnLine.style.background = "#fff";
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
@@ -22,6 +24,7 @@ function toggleMenu() {
         showMenu = true;
     } else {
         menuBtn.classList.remove('close');
+        btnLine.style.background = "#fff";
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
